ae43cff4bf21ad55cd8db7d90e775f8821b56a75
Diego Edenilson Martinez Lopez <ml24002@esfe.agape.edu.sv>
2024-03-21 09:55:21 -0600

    se creo el  DbContext

--- DEMLEYCDAMMAGSMC20240321/Models/ApplicationDBContext.cs
+++ DEMLEYCDAMMAGSMC20240321/Models/ApplicationDBContext.cs
@@ -1,7 +1,17 @@
-﻿namespace DEMLEYCDAMMAGSMC20240321.Models
-{
-    public class ApplicationDBContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace DEMLEYCDAMMAGSMC20240321.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Components> Components { get; set; }
+        public DbSet<Computers> Computers { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Users> Users { get; set; }
     }
+
 }
