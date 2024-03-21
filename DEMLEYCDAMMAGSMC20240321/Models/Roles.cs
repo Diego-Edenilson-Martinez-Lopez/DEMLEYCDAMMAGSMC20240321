fcae0121d2e7cafe22781fe0a5fde2086b42756e
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-03-21 08:33:09 -0600

    clase roles

---
+++ DEMLEYCDAMMAGSMC20240321/Models/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMLEYCDAMMAGSMC20240321.Models
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre debe tener como máximo 50 caracteres.")]
+        public string? Name { get; set; }
+
+        [StringLength(50, ErrorMessage = "La descripción debe tener como máximo 50 caracteres.")]
+        public string? Description { get; set; }
+    }
+
+}
+
