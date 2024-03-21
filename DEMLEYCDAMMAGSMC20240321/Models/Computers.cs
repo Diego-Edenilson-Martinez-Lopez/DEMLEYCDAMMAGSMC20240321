64bee4744d7e5477a97b9cb09041efcb18384817
Diego Edenilson Martinez Lopez <ml24002@esfe.agape.edu.sv>
2024-03-21 09:09:17 -0600

    se creo la clase Computers

--- DEMLEYCDAMMAGSMC20240321/Models/Computers.cs
+++ DEMLEYCDAMMAGSMC20240321/Models/Computers.cs
@@ -1,6 +1,16 @@
-﻿namespace DEMLEYCDAMMAGSMC20240321.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMLEYCDAMMAGSMC20240321.Models
 {
     public class Computers
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "La descripcion es obligatoria.")]
+        public string Brand {  get; set; }
+
+        public virtual ICollection<Components> Components { get; set; } // añadimos la relación
+
     }
 }
